e45d6e26ee74e3098ab76971532633c9ad1ce947
edabrito7 <67392231+edabrito7@users.noreply.github.com>
2020-07-26 13:24:28 -0400

    Created pages & components folders. Components Directory, menuItem created and styled. Homepage created.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Homepage from './pages/homepage/homepage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Crwn-clothing project
+      <Homepage/>
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import DirectoryMenu from '../../components/menu-directory/directory'
+import './homepage.styles.scss'
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <DirectoryMenu />
+        </div>
+    )
+    
+}
+
+
+export default Homepage;
